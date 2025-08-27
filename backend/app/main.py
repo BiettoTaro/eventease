af41f07cf5dc9a1ffcbd4bb6a667af6cae2fd48d
Bietto <fabios90@icloud.com>
2025-08-27 18:42:56 +0100

    Add third party api for fetching tech news

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from app.api import auth, users, events, registrations, news, health
+from app.services.news_provider import fetch_techcrunch_news
+from apscheduler.schedulers.background import BackgroundScheduler
+# from app.services.event_provider import fetch_eventbrite_events
 
 app = FastAPI(title="EventEase API", description="API for EventEase", version="1.0.0")
 
